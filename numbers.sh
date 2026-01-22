9a508d03e4bb76301d1ba6558e9138e99ae2d8a3
JB Larson <68449234+JBLarson@users.noreply.github.com>
2026-01-22 15:41:18 -0800

    add numbers.sh

---
+++ numbers.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# numbers.sh
+# JB Larson
+# CPSC-298 - intro to unix
+
+
+echo "Enter a positive number:"
+read number
+
+for ((i=1; i<=number; i++)); do
+    if (( i % 2 == 0 )); then
+        echo "$i is even"
+    else
+        echo "$i is odd"
+    fi
+done
